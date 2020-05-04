d785f413082b202d33e866454c401d60d8a85861
Irina-zaez <Irina-zaez@yandex.ru>
2020-05-04 22:34:53 +0500

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,10 +1,11 @@
 #include "stdafx.h"
 #include "windows.h"
-#include "Task1.h"
 #include <iostream>
 #include <map>
+#include <set>
 #include <String>
 #include <cctype>
+#include <ctime>
 
 int main()
 {
@@ -12,7 +13,7 @@ int main()
 		FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
 	LARGE_INTEGER size;
 	GetFileSizeEx(myFile, &size);
-	
+
 	std::cout << "Size of file: " << size.QuadPart << "\n";
 
 	DWORD textSize;
@@ -20,22 +21,63 @@ int main()
 	ReadFile(myFile, text, size.QuadPart, &textSize, NULL);
 	CloseHandle(myFile);
 
-	std::map <std::string, int> freqDict;
-	std::string word = "";
-	for (int i = 0; i < size.QuadPart; i++) {
+	clock_t startTime = clock();
+	int start = -1;
+	int lenWord = 0;
+	char* firstWord;
+	std::map<char*, int> firstFreq;
+	for (int i = 0; i < size.QuadPart; i++)
+	{
 		if (isalpha(text[i]))
-			word += tolower(text[i]);
-		else 
-			if (word != "") {
-				if (freqDict.find(word) == freqDict.cend())
-					freqDict[word] = 1;
+		{
+			if (start == -1)
+				start = i;
+			lenWord += 1;
+		}
+		else
+			if (lenWord != 0)
+			{
+				firstWord = (char*)malloc(lenWord);
+				for (int j = 0; j < lenWord; j++)
+				{
+					firstWord[j] = tolower(text[start + j]);
+				}
+				if (firstFreq.find(firstWord) == firstFreq.cend())
+					firstFreq[firstWord] = 1;
 				else
-					freqDict[word]++;
-				word = "";
+					firstFreq[firstWord]++;
+				lenWord = 0;
+				start = -1;
 			}
 	}
+	clock_t endTime = clock();
+
+	int firstTime = ((endTime - startTime) * 1000) / CLOCKS_PER_SEC;
+	std::cout << "First method: " << firstTime << " ms\n";
+
+	startTime = clock();
+	std::map <std::string, int> secondFreq;
+	std::string secondWord = "";
+	for (int i = 0; i < size.QuadPart; i++)
+	{
+		if (isalpha(text[i]))
+			secondWord += tolower(text[i]);
+		else
+			if (secondWord != "")
+			{
+				if (secondFreq.find(secondWord) == secondFreq.cend())
+					secondFreq[secondWord] = 1;
+				else
+					secondFreq[secondWord]++;
+				secondWord = "";
+			}
+	}
+	endTime = clock();
+
+	int secondTime = ((endTime - startTime) * 1000) / CLOCKS_PER_SEC;
+	std::cout << "Second method: " << firstTime << " ms\n";
+
 	std::map<std::string, int>::iterator it;
-	for (it = freqDict.begin(); it != freqDict.end(); it++)
+	for (it = secondFreq.begin(); it != secondFreq.end(); it++)
 		std::cout << it->first << " : " << it->second << '\n';
 }
-
